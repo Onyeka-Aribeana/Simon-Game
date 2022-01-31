0b5706e1776f14bd9427b939c2444d41fbe429b9
Onyeka-Aribeana <77382671+Onyeka-Aribeana@users.noreply.github.com>
2022-01-31 22:34:41 +0100

    Final stage of Simon game

---
+++ game.js
@@ -0,0 +1,64 @@
+const buttonColours = ["red", "blue", "green", "yellow"] 
+let gamePattern = [];
+let userClickedPattern = [];
+let randomChosenColour;
+let startToToggle = false;
+let level = 0;
+ 
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        console.log("sucess");
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){nextSequence()}, 1000);
+        }
+    }
+    
+    else {
+        let audio = new Audio("sounds/wrong.mp3");
+        audio.play();
+        $("body").addClass("game-over");
+        setTimeout(function(){$("body").removeClass("game-over");}, 200)
+        $("h1").text("Game Over, Press any key to restart")
+        level = 0;
+        gamePattern = [];
+        userClickedPattern = [];
+        startToToggle = false; 
+    }
+}
+
+function playSound(name){
+    let audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $(`#${currentColor}`).addClass("pressed");
+    setTimeout(function(){$(`#${currentColor}`).removeClass("pressed");}, 100);
+}
+
+function nextSequence() {
+    userClickedPattern = [];
+    level += 1;
+    $("h1").text("Level " + level);
+
+    let randomNumber = Math.floor(Math.random() * 4);
+    randomChosenColour = buttonColours[randomNumber];
+    playSound(randomChosenColour)
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    gamePattern.push(randomChosenColour);
+}
+
+$(document).keypress(function(){
+    if(startToToggle === false) {
+        nextSequence()
+    }
+    startToToggle = true;
+})
+
+$(".btn").click(function(){
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1)
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+})
